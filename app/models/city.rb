579a4a0fdb1240b666609539683d4f4679b37d22
Joey Bieze <joseph.bieze@gmail.com>
2017-09-21 21:28:47 -0700

    Create City model

---
+++ app/models/city.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: cities
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class City < ApplicationRecord
+  validates :name, presence: true
+
+  has_many :users,
+    primary_key: :id,
+    foreign_key: :user_id,
+    class_name: :User
+end
